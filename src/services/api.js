b7ccc20083e3072df8427b57030848661df43c17
Duceysane26 <duceysane2626@gmail.com>
2026-07-15 23:14:23 +0300

    updated api and .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || 
-'https://localhost:7125/api'; 
+'https://duceysane-001-site1.etempurl.com/api'; 
 
 
 const api = axios.create({ 
