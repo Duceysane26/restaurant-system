9e79bd5ff1fc903e0f5b630e8970010af84603df
Duceysane26 <duceysane2626@gmail.com>
2026-07-19 11:57:25 +0300

    updated api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 
-'https://localhost:7125/api'; 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL; 
 
 
 const api = axios.create({ 
